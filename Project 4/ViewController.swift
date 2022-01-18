3c813fcac20cf75c07ba4e66a7995ab5ecc03a3f
keertirajmalik <keertirajmalik@gmail.com>
2022-01-18 17:43:24 +0530

    Adding the Refresh button and a spacer to push the reload button to right

--- Project 4/ViewController.swift
+++ Project 4/ViewController.swift
@@ -21,6 +21,11 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        toolbarItems = [spacer, refresh]
+        navigationController?.isToolbarHidden = false
         // Do any additional setup after loading the view.
         
         let url = URL(string: "https://www.apple.com")!
